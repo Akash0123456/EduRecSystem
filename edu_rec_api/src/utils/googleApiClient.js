2e13b9524617d2e9afe60a112073b5b36c9914d0
Akash Gampala <agampala@ufl.edu>
2025-03-09 22:13:54 -0400

    Create Google Search API Client with axios

---
+++ edu_rec_api/src/utils/googleApiClient.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+const googleApiClient = axios.create({
+    baseURL: "GET https://customsearch.googleapis.com/customsearch/v1",
+    timeout: 5000
+});
+
+module.exports = googleApiClient;
