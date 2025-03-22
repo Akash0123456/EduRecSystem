a5a68d0b75b7abb485470fc3c33559e72884269a
Akash Gampala <agampala@ufl.edu>
2025-03-22 13:21:33 -0400

    Declare routes for openAI chat API call.

--- edu_rec_api/src/index.js
+++ edu_rec_api/src/index.js
@@ -2,11 +2,15 @@ require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // Define routes
 const googleSearchRoutes = require("./routes/googleSearchRoutes");
+const openaiRoutes = require("./routes/openaiRoutes");
 
 // Create routes
 app.use("/search", googleSearchRoutes);
+app.use("/chat", openaiRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
